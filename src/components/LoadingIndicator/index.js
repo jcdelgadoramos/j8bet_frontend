4176e3d42c887921cfbbce1befa72abaf18baf2c
Jose Carlos Delgado Ramos <jose.delgado@inka-labs.com>
2021-03-15 11:00:58 -0500

    #4 fix in login process

--- src/components/LoadingIndicator/index.js
+++ src/components/LoadingIndicator/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 function LoadingIndicator(props) {
   const { classes } = props;
   return (
-      <h2>Cargando...</h2>
+      <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+      Cargando...
+      </h2>
   )
 };
 
--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -12,7 +12,17 @@ function Login (props) {
   const [submitted, setSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  const [login, { data }] = useMutation(LOGIN_MUTATION);
+  const [login, { data }] = useMutation(LOGIN_MUTATION, {
+    onCompleted: (data) => {
+      setAuthToken(data.tokenAuth.token);
+      setError(false);
+      props.onLogged();
+      setRedirectToReferrer(true);
+    },
+    onError: (error) => {
+      setError(true);
+    },
+  });
   
   if (redirectToReferrer === true) {
     return <Redirect to='/' />
@@ -31,17 +41,8 @@ function Login (props) {
           event.preventDefault();
           login({variables: {
             username: username,
-            password: password
+            password: password,
           }})
-          .then(res => {
-            setAuthToken(data.tokenAuth.token);
-            setError(false);
-            props.onLogged();
-            setRedirectToReferrer(true);
-          })
-          .catch(err => {
-            setError(true);
-          });
         }}>
           <input type="hidden" name="remember" value="true" />
           <div className="rounded-md shadow-sm -space-y-px">
@@ -73,9 +74,9 @@ function Login (props) {
           <div>
             <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                {/* <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                   <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-                </svg> */}
+                </svg>
               </span>
               Ingresa
             </button>
