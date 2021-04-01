54e0bead4e82c28b957b4ad44b744ab83dd2b85a
Jose Carlos Delgado Ramos <jose.delgado@inka-labs.com>
2021-04-01 14:18:16 -0500

    #6 new adminpage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import client from './utils/api'
+import AdminPage from './containers/AdminPage'
 import LoginPage from './containers/Login';
 import HomePage from './containers/HomePage/Loadable';
 import PrivateRoute from './components/PrivateRoute';
@@ -20,6 +21,7 @@ function App(props) {
         <Switch>
           <Route exact path="/login" render={() => (<LoginPage onLogged={handleOnLogged}/>)} />
           <PrivateRoute exact path="/" component={HomePage} />
+          <PrivateRoute exact path="/admin" component={AdminPage} />
         </Switch>
       </BrowserRouter>
     </ApolloProvider>
---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const APP_MANAGER = 'Application Manager';
+export const BET_MANAGER = 'Bet Manager';
+export const BET_CONSUMER = 'Bet Consumer';
