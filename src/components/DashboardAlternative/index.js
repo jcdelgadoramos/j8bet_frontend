cebb69d3a1c60a213637284df5f46f5fb6d99ddb
Jose Carlos Delgado Ramos <jcdelgadoramos@outlook.com>
2021-07-01 21:50:54 -0500

    #12 clickable floating div

--- src/components/DashboardAlternative/index.js
+++ src/components/DashboardAlternative/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function DashboardAlternative(props) {
 	const event = props.event;
+    const [clicked, setClicked] = useState(false);
+
+    function titleClicked(event) {
+        setClicked(!clicked);
+    };
 
 	return (
         <div key={event.id}
-            className="flex flex-col sm:flex-row justify-between
-            p-1 my-1 rounded-md cursor-pointer text-left
-			text-white bg-gray-500 hover:bg-gray-700"
+            className={`flex flex-col sm:flex-row justify-between p-1 my-1
+            rounded-md cursor-pointer text-left select-none hover:bg-gray-700
+            ${clicked ? "bg-gray-700" : "bg-gray-500"} text-white`}
+            onClick={ e => titleClicked(e) }
         >
             <div>{event.name}</div>
             <div className="sm:text-right font-bold">
--- src/components/DashboardEntry/index.js
+++ src/components/DashboardEntry/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import DashboardAlternative from '../DashboardAlternative';
 
 function DashboardEntry(props) {
 	const affair = props.affair;
+    const [modalOpen, setModalOpen] = useState(false);
+    const [clicked, setClicked] = useState(false);
+
+    function titleClicked(event) {
+        setModalOpen(!modalOpen);
+        setClicked(!clicked);
+    };
 
     return (
         <div key={affair.id} className="w-full sm:w-1/2 p-1 text-left">
-            <div className="w-full p-1 my-2 rounded-md cursor-pointer
-                text-gray-200 bg-cyan-800
-                hover:text-cyan-800 hover:bg-gray-200"
+            <div className={`w-full p-1 my-2 rounded-md cursor-pointer
+                select-none hover:text-cyan-800 hover:bg-gray-200
+                ${clicked ?
+                    "text-cyan-800 bg-gray-200" : "text-gray-200 bg-cyan-800"} 
+                `}
+                onClick={ e => titleClicked(e) }
             >
                 {affair.description}
             </div>
@@ -19,6 +29,17 @@ function DashboardEntry(props) {
                     ></DashboardAlternative>
                 ))
             }
+            <div id={affair.id+"Modal"}
+                className={`${modalOpen ? "" : "hidden" } absolute
+                top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+                w-3/4 sm:w-3/5 md:w-3/5 mx-auto rounded-md 
+                bg-coolGray-900 text-gray-200 border-gray-200"`}
+            >
+              <div className="m-2 p-2 rounded-md bg-cyan-800">
+                <span class="close">&times;</span>
+                <p>Some text in the Modal..</p>
+              </div>
+            </div>
         </div>
 	)
 };
